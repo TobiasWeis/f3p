a53847dd90c0f6c9fe42ddbc8ccb9365f414d995
TobiasWeis <mail@tobias-weis.de>
2022-07-22 23:12:46 +0200

    integrated temperature in plots

--- code/app/views/pages.py
+++ code/app/views/pages.py
@@ -5,7 +5,7 @@ import datetime
 from sqlalchemy import and_
 
 from app import db
-from app.models import Timepoint, Club, Course
+from app.models import Timepoint, Club, Course, TimepointWeather
 
 pages_blueprint = Blueprint('pages_blueprint', __name__, url_prefix="", template_folder='../templates/', static_folder='static', static_url_path='../static/')
 
@@ -50,11 +50,16 @@ def detail():
 
     ret = {}
     ret['timepoints'] = []
+    ret['timepoints_weather'] = []
     ret['courses'] = []
 
     timepoints = Timepoint.query.filter(Timepoint.id_club==club.id).order_by(Timepoint.timestamp).all()
     for tp in timepoints:
         ret['timepoints'].append([tp.timestamp, tp.checkins, tp.total_allowed])
+
+    timepoints_weather = TimepointWeather.query.filter(TimepointWeather.id_club==club.id).order_by(TimepointWeather.timestamp).all()
+    for tpw in timepoints_weather:
+        ret['timepoints_weather'].append([tpw.timestamp, tpw.temperature])
 
     courses = Course.query.filter(Course.id_club==club.id).filter(
             and_(
